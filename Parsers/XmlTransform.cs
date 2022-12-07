b8d0bb511eacae62733df1010bb0ecce25999782
Vladyslav Skyba <101654443+vladskyba@users.noreply.github.com>
2022-12-07 22:40:11 +0200

    Transform updates

--- Parsers/XmlTransform.cs
+++ Parsers/XmlTransform.cs
@@ -13,14 +13,6 @@ public static class XmlTransform
         StringWriter results = new StringWriter();
         using(XmlReader reader = XmlReader.Create(new StringReader(inputXml)))
         {
-            var xml = reader.Read();
-            
-            string msg_xml = Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble());
-            if (xml.StartsWith(msg_xml))
-            {
-                xml = xml.Remove(0, msg_xml.Length);
-            }
-            
             transform.Transform(reader, null, results);
         }
 
